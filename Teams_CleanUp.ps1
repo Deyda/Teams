9fb842ebab4511975ca0c3e19eca716183f4c5f4
Deyda <59874195+Deyda@users.noreply.github.com>
2020-03-06 13:12:11 +0100

    Update Teams_CleanUp.ps1

--- Teams_CleanUp.ps1
+++ Teams_CleanUp.ps1
@@ -1,9 +1,17 @@
 <#
 .SYNOPSIS
 This script allows you to uninstall the Microsoft Teams app and remove Teams directory for a user.
+
 .DESCRIPTION
 Use this script to clear the installed Microsoft Teams application. Run this PowerShell script for each user profile for which the Teams App was installed on a machine. After the PowerShell has executed on all user profiles, Teams can be redeployed.
+
+.NOTES
+  Version:        2.0
+  Author:         Manuel Winkel <www.deyda.net>
+  Creation Date:  2020-03-04
+  Purpose/Change: Edit for Version 1.2.00.32462 and newer
 #>
+
 
 $TeamsPath = [System.IO.Path]::Combine($env:LOCALAPPDATA, 'Microsoft', 'Teams')
 $TeamsUpdateExePath = [System.IO.Path]::Combine($env:LOCALAPPDATA, 'Microsoft', 'Teams', 'Update.exe')
